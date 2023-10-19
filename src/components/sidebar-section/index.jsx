fd1bda79c580c06ac563188d23ec510cff9301f0
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-10-19 21:39:21 +0400

    Create footer component

---
+++ src/components/sidebar-section/index.jsx
@@ -0,0 +1,33 @@
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function SidebarSection({ title, children, more }) {
+	return (
+		<section className="bg-[color:var(--background-secondary)] mb-4 rounded-2xl border border-[color:var(--background-secondary)]">
+			<h5 className="py-3 px-4 text-xl font-extrabold leading-6 flex items-center">
+				{title}
+			</h5>
+			<div className="grid">
+				{children}
+			</div>
+			{more && (
+				<Link
+					to={more}
+					className="h-[3.25rem] flex rounded-b-2xl items-center px-4 text-[color:var(--color-primary)] transition-colors hover:bg-[color:var(--background-third)]"
+				>
+					Show more
+				</Link>
+			)}
+		</section>
+	)
+}
+
+SidebarSection.propTypes = {
+	title: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
+	more: PropTypes.oneOfType([PropTypes.bool, PropTypes.string])
+}
+
+SidebarSection.defaultProps = {
+	more: false
+}
---
+++ src/layouts/main/rightbar/footer/index.jsx
@@ -0,0 +1,45 @@
+import {Link} from "react-router-dom";
+import {Popover} from "@headlessui/react";
+
+export default function Footer() {
+	return (
+		<footer className="mb-4 px-4 flex flex-wrap gap-2 text-[0.813rem]">
+			<Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Terms of Service</Link>
+			<Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Terms of Service</Link>
+			<Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Cookie Policy</Link>
+			<Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Accessibility</Link>
+			<Link to="/" className="text-[color:var(--color-base-secondary)] leading-4 hover:underline">Ads info</Link>
+			<div className="inline-flex gap-2 flex-wrap">
+				<Popover className="relative inline-flex leading-4">
+					<Popover.Button
+						className="text-[color:var(--color-base-secondary)] outline-none leading-4 hover:underline inline-flex items-center">
+						More
+						<svg viewBox="0 0 24 24" className="h-[1em] px-0.5">
+								<path
+									fill="currentColor"
+									d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
+								/>
+						</svg>
+					</Popover.Button>
+					<Popover.Panel className="w-[9.375rem] max-w-[24rem] bg-[color:var(--background-primary)] shadow-box rounded-xl absolute bottom-0 right-0 grid overflow-hidden">
+						<Link to="/" className="py-3 px-4 leading-5 font-bold hover:bg-[color:var(--background-secondary)] transition-colors">
+							About
+						</Link>
+						<Link to="/" className="py-3 px-4 leading-5 font-bold hover:bg-[color:var(--background-secondary)] transition-colors">
+							Status
+						</Link>
+						<Link to="/" className="py-3 px-4 leading-5 font-bold hover:bg-[color:var(--background-secondary)] transition-colors">
+							X for Business
+						</Link>
+						<Link to="/" className="py-3 px-4 leading-5 font-bold hover:bg-[color:var(--background-secondary)] transition-colors">
+							Developers
+						</Link>
+					</Popover.Panel>
+				</Popover>
+				<p className="text-[color:var(--color-base-secondary)] leading-4">
+					&copy; 2023 X Corp.
+				</p>
+			</div>
+		</footer>
+	)
+}
