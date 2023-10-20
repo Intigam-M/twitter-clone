4367b67092705acd99a3881ee9095b2d6b64dd13
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-10-20 16:07:36 +0400

    Create user-card component

---
+++ src/components/user-card/index.jsx
@@ -0,0 +1,38 @@
+import Button from "~/components/button";
+import {useState} from "react";
+
+export default function UserCard({user}) {
+
+	const [following, setFollowing] = useState(false)
+
+	return (
+		<button className="py-3 px-4 flex gap-3 transition-colors hover:bg-[color:var(--background-third)]">
+			<div className="flex-1 flex gap-3">
+				<img src={user.avatar} className="w-10 h-10 rounded-full object-cover" alt=""/>
+				<div className="w-[calc(100%-3.25rem)] text-left">
+					<div className="leading-5 font-bold truncate">
+						{user.fullName}
+					</div>
+					<div className="text-[color:var(--color-base-secondary)] truncate">@{user.username}</div>
+				</div>
+			</div>
+			<div>
+				{following ? (
+					<Button
+						variant="white-outline"
+						size="small"
+						onClick={() => setFollowing(false)}
+						className="whitespace-nowrap group"
+					>
+						<div className="flex group-hover:hidden">Following</div>
+						<div className="hidden group-hover:flex">Unfollow</div>
+					</Button>
+				) : (
+					<Button variant="white" size="small" onClick={() => setFollowing(true)}>
+						Follow
+					</Button>
+				)}
+			</div>
+		</button>
+	)
+}
