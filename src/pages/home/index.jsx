8f6e3b23e8200e486c41aa3fa7f38d07ffd01cf6
Intigam Mammadli <intigam.mammadli@gmail.com>
2023-10-25 23:36:19 +0400

    Create sticky-header component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,28 @@
+import StickyHeader from "~/components/sticky-header";
+import Tab from "~/components/tab";
+import ForYou from "~/pages/home/for-you";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+	return (
+		<>
+			<Tab activeTab="for-you">
+				<StickyHeader title="Homepage">
+					<Tab.Items>
+						<Tab.Item id="for-you">
+							For you
+						</Tab.Item>
+						<Tab.Item id="followings">
+                            Followings
+						</Tab.Item>
+					</Tab.Items>
+				</StickyHeader>
+				<Tab.Content id="for-you">
+					<ForYou />
+				</Tab.Content>
+				<Tab.Content id="followings">
+					2. content
+				</Tab.Content>
+			</Tab>
+		</>
+	)
 }
